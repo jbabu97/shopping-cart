0cd46c474b317cce569b70f3f2949d2e64a6c638
Jabbar Babu <jabbar.babu81@gmail.com>
2021-01-29 16:28:18 +0600

    added function for phone and case price

--- script_2.js
+++ script_2.js
@@ -1,22 +1,71 @@
+// for phone increase
 document.getElementById('phone_increment').addEventListener('click', function () {
+    handlePhoneCount(true);
+    
+})
+
+// phone decrease is call from html attribute 'onclick'
+// document.getElementById('phone_decrement').addEventListener('click', function () {
+//     const phoneDecrement = document.getElementById('phone_count');
+//     const phoneCount = parseInt(phoneDecrement.value);
+//     const phoneNewCount = phoneCount - 1;
+//     phoneDecrement.value = phoneNewCount; 
+//     const phonePrice = phoneNewCount * 1219;
+//     document.getElementById('phone_price').innerText = phonePrice;
+// });
+
+function handlePhoneCount(isIncrease) {
     const phoneInput = document.getElementById('phone_count');
     const phoneCount = parseInt(phoneInput.value);
-    const phoneNewCount = phoneCount + 1;
+    let phoneNewCount = phoneCount;
+    if (isIncrease == true) {
+        phoneNewCount = phoneCount + 1;
+    }
+    if (isIncrease == false && phoneCount > 0) {
+        phoneNewCount = phoneCount - 1;
+    }
     phoneInput.value = phoneNewCount;
     const phonePrice = phoneNewCount * 1219;
     document.getElementById('phone_price').innerText = phonePrice;
-})
+}
+
+// for case increase
+document.getElementById('case_increase').addEventListener('click', function () {
+    handleCaseCount(true);
+    // const caseInput = document.getElementById('case_count');
+    // const caseCount = parseInt(caseInput.value);
+    // let newCaseCount = caseCount + 1;
+    // caseInput.value = newCaseCount;
+    // const casePrice = newCaseCount * 59;
+    // document.getElementById('case_price').innerText = casePrice;
+});
 
 
-document.getElementById('phone_decrement').addEventListener('click', function () {
-    const phoneDecrement = document.getElementById('phone_count');
-    const phoneCount = parseInt(phoneDecrement.value);
-    const phoneNewCount = phoneCount - 1;
-    phoneDecrement.value = phoneNewCount; 
-    const phonePrice = phoneNewCount * 1219;
-    document.getElementById('phone_price').innerText = phonePrice;
+// for case decrease
+document.getElementById('case_decrease').addEventListener('click', function () {
+    handleCaseCount(false);
+    // const caseInput = document.getElementById('case_count');
+    // const caseCount = parseInt(caseInput.value);
+    // let newCaseCount = caseCount -1;
+    // caseInput.value = newCaseCount;
+    // const casePrice = newCaseCount * 59;
+    // document.getElementById('case_price').innerText = casePrice;
+});
 
-    if (phoneCount < 1) {
-        return phoneDecrement.value = 0;
+function handleCaseCount(isIncrease) {
+    const caseInput = document.getElementById('case_count');
+    const caseCount = parseInt(caseInput.value);
+    // let newCaseCount = caseCount -1;
+    let newCaseCount = caseCount;
+    if (isIncrease == true) {
+        newCaseCount = caseCount +1;
     }
-})
+    if (isIncrease == false && caseCount > 0) {
+        newCaseCount = caseCount -1;
+    }
+
+    caseInput.value = newCaseCount;
+    const casePrice = newCaseCount * 59;
+    document.getElementById('case_price').innerText = casePrice;
+
+}
